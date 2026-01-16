abec9ba33f28299ad02b48aed06b838a9946a107
esperzmaniac <blob4425@gmail.com>
2026-01-16 11:27:55 +0700

    Some new movement code

--- Scripts/Characters/Hitotsuki/HitotsukiMain.cs
+++ Scripts/Characters/Hitotsuki/HitotsukiMain.cs
@@ -2,6 +2,13 @@ using Godot;
 
 public partial class HitotsukiMain : CharacterBody2D
 {
+    // Assigning objects
+    public override void _Ready()
+    {
+        Ray = GetNode<RayCast2D>("RayCast2D");
+    }
+
+
     public override void _PhysicsProcess(double delta)
     {
         GetInput();
--- Scripts/Characters/Hitotsuki/HitotsukiMovement.cs
+++ Scripts/Characters/Hitotsuki/HitotsukiMovement.cs
@@ -2,9 +2,16 @@ using Godot;
 
 public partial class HitotsukiMain
 {
+	[Export] public int TileSize;
+
+	public RayCast2D Ray;
+	public bool moving = false;
 	public Vector2 InputDir;
+
 	public void GetInput()
 	{
+		if(moving) return;
+
 		InputDir = Vector2.Zero;
 		if (Input.IsActionPressed("ui_up"))
 		{
@@ -16,11 +23,39 @@ public partial class HitotsukiMain
 		}
 		else if(Input.IsActionPressed("ui_left"))
 		{
-			InputDir = new Vector2(1, 0);
+			InputDir = new Vector2(-1, 0);
 		}
 		else if(Input.IsActionPressed("ui_right"))
 		{
-			InputDir = new Vector2(-1, 0);
+			InputDir = new Vector2(1, 0);
 		}
+		if(InputDir != Vector2.Zero)
+		{
+			TryMove();			
+		}
+	}
+
+	public void TryMove()
+	{
+		Ray.TargetPosition = InputDir * TileSize;
+		Ray.ForceRaycastUpdate();
+
+		if(Ray.IsColliding()) return;
+
+		OnMove();
+	}
+
+	public void OnMove()
+	{
+		moving = true;
+
+		var Tween = CreateTween();
+		Tween.TweenProperty(this, "position", Position + InputDir * TileSize, 2.2);
+		Tween.TweenCallback(Callable.From(FinishMove));
+	}
+
+	public void FinishMove()
+	{
+		moving = false;
 	}
 }
