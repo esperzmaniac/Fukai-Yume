2edfd69cbb3e5cf845104a1545059953f5cfa5e1
esperzmaniac <blob4425@gmail.com>
2026-01-21 03:19:52 +0700

    Interaction logic finished

--- Scripts/Objects/Interactable.cs
+++ Scripts/Objects/Interactable.cs
@@ -4,6 +4,9 @@ public partial class Interactable : Sprite2D
 {
 	public Area2D InteractObject;
 	public CustomSignals _customSignals;
+	public CharacterBody2D Hitotsuki;
+	public HitotsukiMain HitoMain;
+	public AudioStreamPlayer Burger;
 	public bool Interact = false;
     public override void _Ready()
 	{
@@ -12,11 +15,15 @@ public partial class Interactable : Sprite2D
 		InteractObject.AreaEntered += OnAreaEntered;
 		InteractObject.AreaExited += OnAreaExited;
 		_customSignals.InteractSignal += Interacted;
+		Hitotsuki = GetNode<CharacterBody2D>("/root/Test Level/Hitotsuki");
+		HitoMain = GetNode<HitotsukiMain>("/root/Test Level/Hitotsuki");
+		Burger = GetNode<AudioStreamPlayer>("CrunchyBite");
 	}
 	
 	public void OnAreaEntered(Area2D area)
 	{
 		Interact = true;
+
 	}
 	public void OnAreaExited(Area2D area)
 	{
@@ -26,7 +33,17 @@ public partial class Interactable : Sprite2D
 	{
 		if (Interact)
 		{
-			GD.Print("worked");
+			HitoMain.moving = true;
+			HitoMain.InputDir = new Vector2(1, 0);
+			HitoMain.Animation.Animation = "Right";
+			Burger.Play();
+			var currentPos = Hitotsuki.Position;
+			var Tween = CreateTween();
+			Tween.SetTrans(Tween.TransitionType.Sine);
+			Tween.TweenProperty(Hitotsuki, "position", Hitotsuki.Position + new Vector2(32, 0), 0.2);
+			this.Visible = false;
+			Tween.TweenProperty(Hitotsuki, "position", currentPos, 0.2);
+			HitoMain.moving = false;
 		}
 	}
 
