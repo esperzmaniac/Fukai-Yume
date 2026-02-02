26ea65f7567a6c001356d8073fb19b44a93320e6
esperzmaniac <Blob4425@gmail.com>
2026-02-02 18:55:36 +0700

    idk what i added

--- Scripts/Characters/Hitotsuki/HitotsukiMain.cs
+++ Scripts/Characters/Hitotsuki/HitotsukiMain.cs
@@ -2,34 +2,34 @@ using Godot;
 
 public partial class HitotsukiMain : CharacterBody2D
 {
-    // Assigning object
-    public override void _Ready()
-    {
-        Ray = GetNode<RayCast2D>("RayCast2D");
-        Animation = GetNode<AnimatedSprite2D>("Animation");
-        InteractionArea = GetNode<Area2D>("Interactions/Area2D");
+	// Assigning object
+	public override void _Ready()
+	{
+		Ray = GetNode<RayCast2D>("RayCast2D");
+		Animation = GetNode<AnimatedSprite2D>("Animation");
+		InteractionArea = GetNode<Area2D>("Interactions/Area2D");
 
-        //Signals
-        InteractionArea.AreaEntered += OnAreaEntered;
-        InteractionArea.AreaExited += OnAreaExited;
-        _customSignals = GetNode<CustomSignals>("/root/CustomSignals");
-    }
+		//Signals
+		InteractionArea.AreaEntered += OnAreaEntered;
+		InteractionArea.AreaExited += OnAreaExited;
+		_customSignals = GetNode<CustomSignals>("/root/CustomSignals");
+	}
 
 
-    public override void _PhysicsProcess(double delta)
-    {
-        GetInput();
-        
-        //Animation while playing logic
-        if(moving) FrameProgressLogic();
-        //lmao
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		
+		//Animation while playing logic
+		if(moving) FrameProgressLogic();
+		//lmao
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        //Interaction
-        Interact();
-    }
+	public override void _Input(InputEvent @event)
+	{
+		//Interaction
+		Interact();
+	}
 }
 
 //happy new system
